a63abf56d7ca5cb1d9b44d4837aca3a06735cab7
Ferran Martínez <ferran.martinez@travelperk.com>
2024-11-28 16:27:46 +0100

    tests(base): Fixed react-testing-library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+describe("asdasd", () => {
+  it("asdasd", () => {
+    expect(1).toBe(1);
+  });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it("bsdsdsv", () => {
+    expect(2).toBe(1);
+  });
 });
