fba4617efdb8e178eb4818f8851d9d10f3c0ff28
Ferran Martínez <ferran.martinez@travelperk.com>
2024-12-12 09:17:49 +0100

    tests(common): Added tests for Dropdown and Loading components

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,13 +1,14 @@
-
-type Props = {
-    children: React.ReactNode;
-    isLoading: boolean;
+export type Props = {
+  children: React.ReactNode;
+  isLoading: boolean;
 };
 
-export const Loading = ({isLoading, children}: Props) => {
-    return (
-        isLoading ? <div>
-            <h1>Loading...</h1>
-        </div> : <>{children}</>
-    );
-}
+export const Loading = ({ isLoading, children }: Props) => {
+  return isLoading ? (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  ) : (
+    <>{children}</>
+  );
+};
